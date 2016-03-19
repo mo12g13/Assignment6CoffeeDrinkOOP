c8f11875392902d6f21779a2130369e4b4fec0ee
Momo Johnson <mo12g13@gmail.com>
2016-03-18 22:26:23 -0500

    Comment added.

--- src/com/Momo/CofeeDrink.java
+++ src/com/Momo/CofeeDrink.java
@@ -8,7 +8,7 @@ public class CofeeDrink  {
     protected String coffeeName;
     protected double costToMake;
     protected double price;
-
+//Initiliazing various element to the constructor
     public CofeeDrink(String coffeeName, double costToMake, double price){
         this.coffeeName = coffeeName;
         this.costToMake = costToMake;
@@ -16,33 +16,36 @@ public class CofeeDrink  {
 
     }
 
-
+//A method that calculates the expenses
    public double expenses(int amountsold){
        double revsale = amountsold * costToMake;
        return revsale;
    }
 
+    //A method that calculates the revenue
     public double revenue(int amountSold){
         double expsale = amountSold*price;
         return expsale;
     }
 
+    // A method that return the name of the coffee
     public String getCoffeeName() {
         return coffeeName;
     }
-
+//A method that sets the name of the coffee
     public void setCoffeeName(String coffeeName) {
         this.coffeeName = coffeeName;
     }
-
+//A method that get the cost to make a coffee
     public double getCostToMake() {
         return costToMake;
     }
-
+//A methos that sets the cost to make coffee
     public void setCostToMake(double costToMake) {
         this.costToMake = costToMake;
     }
 
+    // A method that gets a the price to make a coffee
     public double getPriceTomake() {
         return price;
     }
@@ -50,12 +53,13 @@ public class CofeeDrink  {
     public void Price(double priceTomake) {
         this.price = priceTomake;
     }
-
+//A method that calculates the profit
     public double profit(double revenue, double expenses){
         double coffeeProfit = revenue -expenses;
         return coffeeProfit;
     }
 
+    // A method that does input validation in order to make sure the user enter the right input
     public static int amountSold(String cupSolds){
         Scanner input = new Scanner(System.in);
         System.out.println("How many "+ cupSolds + " "+ "did sold today: ");
--- src/com/Momo/CoffeeDrinkManager.java
+++ src/com/Momo/CoffeeDrinkManager.java
@@ -3,39 +3,37 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class CoffeeDrinkManager {
-
-
-
     public static void main(String[] args) {
+        //Declaring of variables to be used in my program
         FileWriter saleReport;
         String[] lineSplit;
         String coffeeName;
-        double priceToMake, costToMake,  totalRevenue=0, totalExpenses=0, totalProfit=0;
 
+        double price, costToMake,  totalRevenue=0, totalExpenses=0, totalProfit=0;
 
         try {
+            //A BufferedReader that reads the coffee.txt file from my directory
             BufferedReader read = new BufferedReader(new FileReader("coffee.txt"));
-            String line = read.readLine();
-            LinkedList<CofeeDrink> myShop = new LinkedList<>();
+            String line = read.readLine();//A method that reads the each line of the file
+            LinkedList<CofeeDrink> myShop = new LinkedList<>();//Initialing a linkedList and adding the CoffeeDrink as my Data type
             while (line != null) {
-                lineSplit = line.split(";");
-                coffeeName = lineSplit[0];
-                costToMake = Double.parseDouble(lineSplit[1]);
-                priceToMake = Double.parseDouble(lineSplit[2]);
-                myShop.add(new CofeeDrink(coffeeName, costToMake, priceToMake));
+                lineSplit = line.split(";");//Splinting of the line in order to get each item out.
+                coffeeName = lineSplit[0];//Assigning the coffee 1st item coffee name to the variable coffeeName
+                costToMake = Double.parseDouble(lineSplit[1]);//parsing and assigning the 2nd item cost to make to variable costToMake
+                price = Double.parseDouble(lineSplit[2]);////Assigning the third item price to the variable price
+                myShop.add(new CofeeDrink(coffeeName, costToMake, price));//Adding my CoffeeDrink Object to my LinkList Object
 
                 line = read.readLine();
             }
 
         read.close();
 
-            saleReport = new FileWriter("SalesReport");
-        for(CofeeDrink ob: myShop){
+            saleReport = new FileWriter("SalesReport");//Inititalization of the saleReport File
+        for(CofeeDrink ob: myShop){// Accessing my object from my linkLinked object and getting values of each item.
             String nameOfCoffee = ob.getCoffeeName();
             double costToMakeCoffee = ob.getCostToMake();
             double priceCharge = ob.getPriceTomake();
@@ -46,24 +44,14 @@ public class CoffeeDrinkManager {
             totalExpenses += toMakeExp;
             totalRevenue +=toChargeRev;
             totalProfit +=profit;
-            System.out.println(profit);
-           //System.out.println(totalExpenses);
-            System.out.println( toMakeExp);
-           //System.out.println(toChargeRev);
-
-
+    //Writing to the salesReport file the necessary information that is needed to be written to the file.
             saleReport.write(String.format("%s: Sold %d, Expenses: $%.2f, Revenue: $%.2f, Profit: $%.2f%n%n", nameOfCoffee,
 
                     totalItemSold, toMakeExp, toChargeRev, profit));
-
-
         }
 
             saleReport.write(String.format("Total: Expenses: $%.2f%nTotal Revenue: $%.2f%nTotal Profit: $%.2f%n",
                     totalExpenses, totalRevenue,totalProfit ));
-
-
-
             saleReport.close();
 
 
@@ -83,91 +71,6 @@ public class CoffeeDrinkManager {
 
 
 
-
-
-
-
-       /* Scanner input = new Scanner(System.in);
-        boolean istrue = true;
-        String teaName;
-        Double priceOfTea;
-        int numberItem=0;
-        double totalProfit =0.0;
-        double revenueTotal =0.0;
-        double expensesTotal = 0.0;
-        double total1 =0.0;
-        double total2 =0.0;
-
-
-        String[] lineSeperate;
-        Double cost;
-
-        try {
-            //Reading of the coffee file by the reader
-            FileReader readThisFile = new FileReader("coffee.txt");
-            // reading of the filereader file by the bufferReader
-            BufferedReader report = new BufferedReader(readThisFile);
-            String line = report.readLine();
-            FileWriter mySalesReport = new FileWriter("saleReport.txt");
-
-            while (line != null) {
-                //Assigning the variable for the coffee.txt
-                lineSeperate = line.split(";");
-                teaName = lineSeperate[0];
-                String line2 = lineSeperate[1];
-                cost = Double.parseDouble(line2);
-                String line3 = lineSeperate[2];
-                priceOfTea = Double.parseDouble(line3);
-
-                do {
-                    try {
-                        System.out.println("How many " + teaName + " did you today?");
-
-                        numberItem = input.nextInt();
-
-
-                        istrue = false;
-
-                    } catch (InputMismatchException e) {
-                        input.nextLine();
-                        System.out.println("Please enter right number");
-                        istrue = true;
-                    }
-                    catch (NoSuchElementException e){
-                        System.out.println(" Please enter positive integers");
-                    }
-                }while (istrue);
-                mySalesReport.write(String.format("%s sold: %d, Expenses: $%.2f, Revenue: $%.2f, Profit: $%.2f%n", teaName,
-                        numberItem, expenses(cost, numberItem),  totalRevnue(priceOfTea, numberItem ), totalRevnue(priceOfTea, numberItem)-expenses(cost,numberItem) ));
-                total1 = totalRevnue(priceOfTea, numberItem);
-                revenueTotal = revenueTotal+total1;
-                total2  = expenses(cost, numberItem);
-                expensesTotal = expensesTotal+total2;
-                totalProfit = revenueTotal-expensesTotal;
-                line = report.readLine();
-            }
-            mySalesReport.write(String.format("Total Revenue: $%.2f%n", revenueTotal ));
-            mySalesReport.write(String.format("Total Expenses: $%.2f%n", expensesTotal));
-            mySalesReport.write(String.format("Total Profit:  $%.2f%n",  totalProfit));
-
-            mySalesReport.close();
-            report.close();
-        } catch (IOException e) {
-            System.out.println("File not found");
-        }
-    }
-    //A method that calculates and returns the expenses
-    public static double expenses(double cost, int typeSold ){
-        double totalExpeses = cost*typeSold;
-        return totalExpeses;
-    }
-    //A method that calculate and returns the total revenue.
-    public static  double totalRevnue(Double price, int amountSold){
-        double totalRev = price*amountSold;
-        return totalRev;
-
-
-    }*/
 
 
 
