8f91fab6f52da17555d7ba82752a6663bf38cd5f
Momo Johnson <mo12g13@gmail.com>
2016-03-18 22:02:12 -0500

    Done! Only need to add comments to my code.

--- src/com/Momo/CoffeeDrinkManager.java
+++ src/com/Momo/CoffeeDrinkManager.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.InputMismatchException;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -15,11 +15,9 @@ public class CoffeeDrinkManager {
         FileWriter saleReport;
         String[] lineSplit;
         String coffeeName;
-        double priceToMake;
-        double costTomake;
-        double totalRevenue =0;
-        double totalExpenses = 0;
-        double totalProfit = 0;
+        double priceToMake, costToMake,  totalRevenue=0, totalExpenses=0, totalProfit=0;
+
+
         try {
             BufferedReader read = new BufferedReader(new FileReader("coffee.txt"));
             String line = read.readLine();
@@ -27,13 +25,11 @@ public class CoffeeDrinkManager {
             while (line != null) {
                 lineSplit = line.split(";");
                 coffeeName = lineSplit[0];
-                costTomake = Double.parseDouble(lineSplit[1]);
+                costToMake = Double.parseDouble(lineSplit[1]);
                 priceToMake = Double.parseDouble(lineSplit[2]);
-                myShop.add(new CofeeDrink(coffeeName, costTomake, priceToMake));
+                myShop.add(new CofeeDrink(coffeeName, costToMake, priceToMake));
 
                 line = read.readLine();
-
-
             }
 
         read.close();
@@ -49,17 +45,22 @@ public class CoffeeDrinkManager {
             double profit = ob.profit(toChargeRev,toMakeExp);
             totalExpenses += toMakeExp;
             totalRevenue +=toChargeRev;
+            totalProfit +=profit;
             System.out.println(profit);
            //System.out.println(totalExpenses);
             System.out.println( toMakeExp);
            //System.out.println(toChargeRev);
 
 
-            saleReport.write(String.format("%s: Sold %d, Expenses: $%.2f, Revenue: $%.2f, Profit: $%.2f%n", nameOfCoffee,
+            saleReport.write(String.format("%s: Sold %d, Expenses: $%.2f, Revenue: $%.2f, Profit: $%.2f%n%n", nameOfCoffee,
+
                     totalItemSold, toMakeExp, toChargeRev, profit));
 
 
         }
+
+            saleReport.write(String.format("Total: Expenses: $%.2f%nTotal Revenue: $%.2f%nTotal Profit: $%.2f%n",
+                    totalExpenses, totalRevenue,totalProfit ));
 
 
 
